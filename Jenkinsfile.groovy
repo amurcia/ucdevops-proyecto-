41146c6e9b4b04a0f07c04d2c2f41f2a9956b751
Alvaro M <amurciac@usuarios-MacBook-Pro.local>
2021-03-02 10:56:27 -0600

    Refactorizando codigo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,37 +2,47 @@ pipeline {
     agent any
 
     stages {
-        stage('Checkout'){
+        stage('Checkout Codigo Fuente') {
 
-            steps{
-                script{
+            steps {
+                script {
+                    try {
 
-                    checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github', url: 'git@github.com:amurcia/ucdevops-proyecto-.git']]])
+                        checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github', url: 'git@github.com:amurcia/ucdevops-proyecto-.git']]])
 
+                    } catch (all) {
+                        currentBuild.result = 'FAILURE'
+                    }
 
                 }
             }
 
         }
 
-stage('Sonarqube') {
-    environment {
-        scannerHome = tool 'SonarQubeScanner'
-    }
-    steps {
-        withSonarQubeEnv('sonarqube') {
+        stage('Sonarqube') {
+            environment {
+                scannerHome = tool 'SonarQubeScanner'
+            }
+            steps {
+                withSonarQubeEnv('sonarqube') {
 
-            sh '''${scannerHome}/bin/sonar-scanner \
+                    sh '''${scannerHome}/bin/sonar-scanner \
                 -Dsonar.projectKey=ucdevops-proyecto \
                 -Dsonar.sources=. \
                 -Dsonar.host.url=http://sandbox.priv:9090 \
                 -Dsonar.login=admin \
                 -Dsonar.password=1q2w3e4r'''
+                }
+
+            }
         }
 
     }
-}
-
+    post {
+        always {
+            echo 'Fin del Procesdo de Pipeline'
+            deleteDir()
+        }
     }
 }
 
